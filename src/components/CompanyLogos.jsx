f3a94d5fe9648ad85c810863cc7ab33f41de4124
Alexandre Vitorino Martins de Lima <alexandrevmartinsdelima@gmail.com>
2024-04-02 11:55:47 -0300

    finalizando Hero Section

--- src/components/CompanyLogos.jsx
+++ src/components/CompanyLogos.jsx
@@ -1,9 +1,22 @@
+import { companyLogos } from "../constants";
+
 export function CompanyLogos({ className }) {
   return (
     <div className={className}>
       <h5 className="tagline mb-6 text-center text-n-1/50">
         Ajudando as pessoas a criar conteúdo incrível em
       </h5>
+
+      <ul className="flex">
+        {companyLogos.map((logo, index) => (
+          <li
+            key={index}
+            className="flex items-center justify-center flex-1 h-[8.5rem]"
+          >
+            <img src={logo} width={134} height={134} alt={logo} />
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
